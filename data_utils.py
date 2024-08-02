57d9333e0e7ed6d4c24aa9f0e8b738372fd7c9b3
bk-anupam <bk_anupam@outlook.com>
2024-08-02 14:01:29 +0530

    updates

--- data_utils.py
+++ data_utils.py
@@ -146,3 +146,37 @@ def plot_feature_importance(df_feature_imp, fig_size=(18, 6)):
     plt.ylabel("Feature Importance")
     # Show the plot
     plt.show()
+
+def reduce_df_memory(df: pd.DataFrame):
+    "This method reduces memory for numeric columns in the dataframe"
+    
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64', "uint16", "uint32", "uint64"]
+    start_mem = df.memory_usage().sum() / 1024**2
+    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+
+            if "int" in str(col_type):
+                if c_min >= np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min >= np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min >= np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min >= np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min >= np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                if c_min >= np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)  
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print(f"Start - end memory:- {start_mem:5.2f} - {end_mem:5.2f} Mb")
+    return df    
